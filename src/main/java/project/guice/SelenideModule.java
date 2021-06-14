7831e09bbf38b5efb1aa2e86ea9f7e71663af305
Vitalii Malinochka <v.malinochka@astoundcommerce.com>
2021-06-14 10:08:52 +0300

    Turn off PageListener

--- src/main/java/project/guice/SelenideModule.java
+++ src/main/java/project/guice/SelenideModule.java
@@ -1,7 +1,6 @@
 package project.guice;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.matcher.Matchers;
 import project.utils.Email;
 import project.utils.EmailProvider;
 
@@ -9,7 +8,7 @@ public class SelenideModule extends AbstractModule {
 
 	@Override 
 	protected void configure() {
-		bindListener(Matchers.any(), new PageListener());
+		//bindListener(Matchers.any(), new PageListener());
 
 		bind(String.class).annotatedWith(Email.class).toProvider(EmailProvider.class);;
 	}
